4aa8b7c6f14dbc9a50b51ee63cb1c295402dfb59
MuChen <752572065@qq.com>
2016-04-14 22:43:12 +0800

    大数LCM
    求分数LCM，并且用上大数。

--- poj2981/poj2981/源.cpp
+++ poj2981/poj2981/源.cpp
@@ -28,72 +28,62 @@ const ld PI = acos(-1.0);
 using namespace std;
 #define SIZE (5000000+10 )
 
-ll a[SIZE];
+ll m[SIZE];
 ll r[SIZE];
 
 ll gcd(ll a, ll b) {
 	return b == 0 ? a : gcd(b,a%b) ;
 }
 
-ll one_gcd(ll a, ll b, ll& c, ll& d) {
-	if (a%b == 0) {
-		c = 1; d = 0;
-		return b;
+void Exgcd(ll a, ll b, ll& d, ll& x, ll& y) {
+	if (!b) {
+		d = a;x = 1;y = 0;
+	} else {
+		Exgcd(b, a%b, d, y, x);
+		y -= x*(a / b);
 	}
-	ll k = one_gcd(b, a%b, c, d);
-	ll tem = d; d = c; c = tem - c*(a / b);
-	return k;
-}
-
-ll one_solve(ll a,ll b,ll l) {
-	ll c, d;
-	ll k = one_gcd(a, l, c, d);
-	if (k != c*a + d*l) {
-		c = d;
-	}
-	ll ans = (b*c) % l;
-	if (ans < 0)
-		ans = (ans + l) % l;
-	return ans;
 }
 
 int main() {
 	//ios_base::sync_with_stdio(false); cin.tie(0);
 	int n;
 	while (~scanf("%d",&n)) {
-		int i,j;
+		ll i,j;
 		ll M = 1;
 		f(i, 1, n) {
-			scanf("%I64d%I64d", &a[i], &r[i]);
-			M *= a[i];
+			scanf("%I64d%I64d", &m[i], &r[i]);
 		}
-
-		/*if (1 == n) {
-			printf("%I64d\n")
-		}*/
-
+		ll a = m[1], b = r[1];
 		int ok = 1;
-		f(i, 1, n) {
-			f(j, i + 1, n)
-				if (1 != gcd(a[i], a[j])) {
-					ok = 0;
-					break;
-				}
-			if (!ok)break;
+		f(i, 2, n) {
+			ll k1, k2;
+			ll p = gcd(a, m[i]);
+			if ((r[i] - b)%p) {
+				ok = 0;
+				break;
+			}
+			ll d;
+			Exgcd(a, m[i], d, k1, k2);
+			k1 = (r[i] - b) / p*k1%(m[i]/p);
+			k2 = (r[i] - b) / p*k2%(a/p);
+			b += k1*a;
+			a = a*(m[i]) / p;
+			b%=a;
 		}
-		if (!ok) printf("-1\n");
-		else {
-			ll ans = 0;
-			f(i, 1, n) {
-				ll tem = M / a[i];
-				ll k = one_solve(tem, 1, a[i]);
-				ans = (ans + r[i] * tem*k%M) % M;
+		ll ans;
+		if (ok) {
+			ans = b;
+			if (ans < 0) {
+				ans += a;
 			}
 			printf("%I64d\n", ans);
-		}
+		} else
+			printf("-1\n");
+
+
+		
 	}
-
-
+	
 
 
 
---
+++ 实验1/实验1/源.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct treenode *bintree;
+// typedef bintree position;
+
+typedef struct treenode {
+	int data;
+	struct treenode *left;
+	struct treenode *right;
+} treenode, *bintree;
+
+void preorderTraversal(bintree bt);
+bintree insert(int x, bintree bt);
+
+int main() {
+	struct treenode *bit;
+	int i, x;
+	bit = NULL;
+	int n;
+	scanf("%d", &n);
+
+	for (i = 0; i<n; i++) {
+		scanf("%d", &x);
+		//	printf("%d\n",x);
+		bit = insert(x, bit);
+		//	 printf("%d",bit->data);
+	}
+	preorderTraversal(bit);
+}
+
+void preorderTraversal(bintree bt) {
+	if (bt!=NULL) {
+		preorderTraversal(bt->left);
+		printf("%d ", bt->data);
+		preorderTraversal(bt->right);
+	}
+}
+bintree insert(int x, bintree bt) {
+	if (!bt) {
+		bt = new struct treenode;
+		//bt = (bintree)malloc(sizeof(struct treenode));
+	
+		if (bt)
+			bt->data = x;
+		else
+			return NULL;
+		bt->left = bt->right = NULL;
+	} else if (x < bt->data) {
+		bt->left = insert(x, bt->left);
+	} else if (x > bt->data) {
+		bt->right = insert(x, bt->right);
+	}
+	//	printf("%d",x);
+	return bt;
+}
